6f03f999ed35c91d2db3c1f3026278acc25dc85d
Gabriel Neil <gabrielneil7@gmail.com>
2024-09-27 14:32:07 +0200

    feature: image classification

--- src/multi_model.py
+++ src/multi_model.py
@@ -1,11 +1,4 @@
-from typing import Dict
-
-from ray import serve
-from ray.serve.handle import DeploymentHandle
-from starlette.requests import Request
-from transformers import pipeline
-
-
+"""
 @serve.deployment(name="Translator_model",
                   num_replicas=2,
                   ray_actor_options={"num_cpus": 0.2, "num_gpus": 0},
@@ -37,8 +30,9 @@ class Translator:
             self.model = pipeline("translation_en_to_ro", model="t5-small")
         else:
             pass
+"""
 
-
+"""
 @serve.deployment(name="Summarizer_model",
                   num_replicas=2,
                   ray_actor_options={"num_cpus": 0.2, "num_gpus": 0},
@@ -78,3 +72,49 @@ class Summarizer:
 
 
 app = Summarizer.bind(Translator.bind())
+"""
+
+from io import BytesIO
+
+import requests
+import starlette
+from PIL import Image
+from ray import serve
+from ray.serve.handle import DeploymentHandle
+from transformers import pipeline
+
+
+@serve.deployment
+def downloader(image_url: str):
+    image_bytes = requests.get(image_url).content
+    image = Image.open(BytesIO(image_bytes)).convert("RGB")
+    return image
+
+
+@serve.deployment(name="ImageClassifier",
+                  num_replicas=2,
+                  ray_actor_options={"num_cpus": 0.2, "num_gpus": 0},
+                  max_ongoing_requests=100,
+                  health_check_period_s=10,
+                  health_check_timeout_s=30,
+                  graceful_shutdown_timeout_s=20,
+                  graceful_shutdown_wait_loop_s=2)
+class ImageClassifier:
+    def __init__(self, downloader: DeploymentHandle):
+        self.downloader = downloader
+        self.model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+
+    async def classify(self, image_url: str) -> str:
+        image = await self.downloader.remote(image_url)
+        results = self.model(image)
+        return results
+
+    async def __call__(self, req: starlette.requests.Request):
+        req = await req.json()
+        return await self.classify(req["image_url"])
+
+
+app = ImageClassifier.options(route_prefix="/classify").bind(downloader.bind())
+
+
+
